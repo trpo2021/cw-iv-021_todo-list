774f78eecf8ab8ca94f59e11782bc18aec957311
Paul Taskaev <Gledex@users.noreply.github.com>
2021-05-09 19:28:28 +0700

    header for Disabling/Enabling button

--- TextField/note.h
+++ TextField/note.h
@@ -1,14 +1,15 @@
 #ifndef NOTE_H
 #define NOTE_H
 
-#include <QDialog>
 #include "ui_note.h"
+#include <QDialog>
 
-class Note:public QDialog, public Ui_Form
-{
+class Note : public QDialog, public Ui_Form {
     Q_OBJECT
 public:
-    Note(QWidget *parent = 0);
+    Note(QWidget* parent = 0);
+private slots:
+    void on_textEdit_textChanged();
 };
 
 #endif // NOTE_H
