521f3a528219147bcd065f43e6bb8a0f0d9789c9
Paul Taskaev <Gledex@users.noreply.github.com>
2021-05-09 19:26:44 +0700

    Enabling/Disabling saveButton

--- TextField/note.cpp
+++ TextField/note.cpp
@@ -1,8 +1,16 @@
 
-#include <QtWidgets>
 #include "note.h"
-Note::Note(QWidget *parent)
-    :QDialog(parent)
+#include <QtWidgets>
+Note::Note(QWidget* parent) : QDialog(parent)
 {
     setupUi(this);
+}
+void Note::on_textEdit_textChanged()
+{
+    QString str = textEdit->toPlainText();
+    if (str != "") {
+        saveButton->setEnabled(true);
+    } else {
+        saveButton->setEnabled(false);
+    }
 }
