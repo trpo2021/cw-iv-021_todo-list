063508b985f1346f0b3b19360b5531c63cb44e04
Paul Taskaev <Gledex@users.noreply.github.com>
2021-05-29 13:26:36 +0700

    Fix building errors

--- ToDo/note.h
+++ ToDo/note.h
@@ -2,9 +2,9 @@
 #define NOTE_H
 
 #include "ui_note.h"
+#include <QDebug>
 #include <QDialog>
 #include <todofile.h>
-#include <QDebug>
 class Note : public QDialog, public Ui_Form {
     Q_OBJECT
 public:
@@ -13,12 +13,10 @@ public:
     bool dead_state = false;
 public slots:
     void redact(QStringList data);
+
 private:
     ToDoFile WriteEdit;
     QString created;
-    
-        };
-
 
 private slots:
     void on_textEdit_textChanged();
@@ -26,8 +24,6 @@ private slots:
     void on_delButton_clicked();
     void on_deadline_currentIndexChanged(int index);
     void on_backButton_clicked();
-
-
 };
 
 #endif // NOTE_H
--- ToDoFile/todofile.h
+++ ToDoFile/todofile.h
@@ -1,15 +1,24 @@
 #ifndef TODOFILE_H
 #define TODOFILE_H
+#include <QString>
 
-
-class ToDoFile
-{
+class ToDoFile {
 public:
     ToDoFile();
-    int write_csv(QString filepath,QStringList data,QString sep,QString delim);
-    int edit_csv(QString filepath,QStringList data,QString created,QString sep,QString delim);
-    int read_csv(QString filepath,QList<QStringList>* readData,QString sep,QString delim);
-    int delete_note(QString filepath,QString created,QString sep,QString delim);
+    int
+    write_csv(QString filepath, QStringList data, QString sep, QString delim);
+    int edit_csv(
+            QString filepath,
+            QStringList data,
+            QString created,
+            QString sep,
+            QString delim);
+    int read_csv(
+            QString filepath,
+            QList<QStringList>* readData,
+            QString sep,
+            QString delim);
+    int
+    delete_note(QString filepath, QString created, QString sep, QString delim);
 };
-
 #endif // TODOFILE_H
